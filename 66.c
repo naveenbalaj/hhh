15f228a1484d47868182aecc8d234d0b01bddd81
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-03-09 16:39:42 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int a,i,p=0;
+printf("value of a:");
+scanf("%d",&a);
+for(a=1;a<=i;a++)
+if(i%a==0)
+p++;
+if(p==2)
+printf("yes");
+else
+printf("no");
+return 0;
+}
+
+
