043b89d0a78d88f3196c43dbe90779001766a869
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-03-20 16:44:35 +0530

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main() 
+{
+char str[50];
+printf("enter the string:");
+scanf("%s",str);
+if(strrev(str[50])=str)	
+printf("isomorphism");
+else
+printf("not isomorphism");
+return 0;
+}
