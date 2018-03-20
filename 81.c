2d497b3e446638b485377a50ed335275b62301b3
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-03-20 16:18:50 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+int a,n2,i;
+printf("\nenter the two numbers:");
+for(i=1;i<=3;i++)
+{
+scanf("%d %d\n",&a,&b;
+if(a>b)
+{
+printf("\n%d",a-b);
+}
+else
+{
+printf("\n%d",a-b);
+}
+}
+return 0;
+}8
